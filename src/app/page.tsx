ed037d651e74fd4cf2239aae45ecbd7a6b6b46ee
samunyberg <samu.nyberg@gmail.com>
2024-03-12 13:41:04 +0200

    Build the Intro component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Intro from '@/components/Intro';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="snap-y snap-mandatory">
+      <div className="h-[calc(100vh-3.5rem)] snap-start">
+        <Intro />
+      </div>
+      <div className="h-screen snap-start bg-bgSoft">
+        <div className="container mx-auto">Instagram Section</div>
+      </div>
+    </div>
+  );
 }
