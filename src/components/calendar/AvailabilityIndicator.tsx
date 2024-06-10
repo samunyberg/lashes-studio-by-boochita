9ac7c22342a0c6ba7fa7620a1da1f0a67b89d4fe
samunyberg <samu.nyberg@gmail.com>
2024-06-10 20:31:06 +0300

    Improve availability indicator

--- src/components/calendar/AvailabilityIndicator.tsx
+++ src/components/calendar/AvailabilityIndicator.tsx
@@ -1,6 +1,25 @@
-const AvailabilityIndicator = () => {
+import { FaCheckCircle } from 'react-icons/fa';
+import { FaBan } from 'react-icons/fa';
+
+interface Props {
+  dayHasAvailableAppointments: boolean;
+  isPassedDay: boolean;
+}
+
+const AvailabilityIndicator = ({
+  dayHasAvailableAppointments,
+  isPassedDay,
+}: Props) => {
   return (
-    <div className='size-2 rounded-full bg-green-400 shadow-sm shadow-gray-500' />
+    <>
+      {dayHasAvailableAppointments && !isPassedDay && (
+        <FaCheckCircle className='size-3 text-green-400' />
+      )}
+
+      {!dayHasAvailableAppointments && !isPassedDay && (
+        <FaBan className='size-3 text-red-400' />
+      )}
+    </>
   );
 };
 
--- src/components/calendar/Legend.tsx
+++ src/components/calendar/Legend.tsx
@@ -2,9 +2,23 @@ import AvailabilityIndicator from './AvailabilityIndicator';
 
 const Legend = () => {
   return (
-    <div className='my-4 flex max-w-fit items-center gap-4'>
-      <AvailabilityIndicator />
-      <span>Available times left</span>
+    <div className='my-4 ml-4 flex max-w-fit flex-col gap-2 text-sm'>
+      <div>
+        <div className='flex items-center gap-2'>
+          <AvailabilityIndicator
+            dayHasAvailableAppointments={true}
+            isPassedDay={false}
+          />
+          <span>Available times left</span>
+        </div>
+        <div className='flex items-center gap-2'>
+          <AvailabilityIndicator
+            dayHasAvailableAppointments={false}
+            isPassedDay={false}
+          />
+          <span>No available times for this date</span>
+        </div>
+      </div>
     </div>
   );
 };
