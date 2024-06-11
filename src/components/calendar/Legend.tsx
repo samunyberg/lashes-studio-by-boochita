e301ad331d837f5c93686d9361664b8536f99f4a
samunyberg <samu.nyberg@gmail.com>
2024-06-11 11:55:49 +0300

    Improve calendar legend

--- src/components/calendar/Legend.tsx
+++ src/components/calendar/Legend.tsx
@@ -2,16 +2,19 @@ import AvailabilityIndicator from './AvailabilityIndicator';
 
 const Legend = () => {
   return (
-    <div className='my-4 ml-4 flex max-w-fit flex-col gap-2 text-sm'>
+    <div className='my-4 flex flex-col gap-2 bg-bgSoft p-3 text-sm shadow'>
       <div>
-        <div className='flex items-center gap-2'>
+        <div className='mb-3'>
+          Click a date in calendar to see appointment times.
+        </div>
+        <div className='ml-4 flex items-center gap-2'>
           <AvailabilityIndicator
             dayHasAvailableAppointments={true}
             isPassedDay={false}
           />
           <span>Available times left</span>
         </div>
-        <div className='flex items-center gap-2'>
+        <div className='ml-4 flex items-center gap-2'>
           <AvailabilityIndicator
             dayHasAvailableAppointments={false}
             isPassedDay={false}
