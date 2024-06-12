dce07df51d26951a83ebbcb030fef84d48a8fbd2
samunyberg <samu.nyberg@gmail.com>
2024-06-12 20:07:32 +0300

    Make CheckBox more reusable

--- src/components/common/CheckBox.tsx
+++ src/components/common/CheckBox.tsx
@@ -4,14 +4,15 @@ import { FaCheck } from 'react-icons/fa6';
 
 interface Props {
   isChecked: boolean;
+  className?: string;
 }
 
-const CheckBox = ({ isChecked }: Props) => {
+const CheckBox = ({ isChecked, className }: Props) => {
   return (
     <div
       className={`${
         isChecked && '!bg-accent'
-      } flex size-5 cursor-pointer items-center justify-center rounded-sm border border-primary bg-white/50 transition-all`}
+      } flex size-5 cursor-pointer items-center justify-center rounded-sm border border-primary transition-all ${className ? className : ''}`}
     >
       {isChecked && <FaCheck className='scale-100 text-white' />}
     </div>
