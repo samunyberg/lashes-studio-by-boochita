910fae78548c321afaea54f9fc7b162280842b7e
samunyberg <samu.nyberg@gmail.com>
2024-06-13 12:54:27 +0300

    Add label to Input component

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,13 +1,28 @@
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string;
+  id?: string;
+  label?: string;
   placeholder: string;
   error?: string;
 }
 
-const Input = ({ className, placeholder, error, ...rest }: Props) => {
+const Input = ({
+  className,
+  id,
+  label,
+  placeholder,
+  error,
+  ...rest
+}: Props) => {
   return (
     <div className='flex flex-col gap-2'>
+      {label && (
+        <label htmlFor={id} className='text-sm'>
+          {label}
+        </label>
+      )}
       <input
+        id={id}
         className={`rounded-sm p-2 shadow placeholder:text-sm focus:outline-2 focus:outline-accent ${error && 'outline outline-red-300'} ${className && className}`}
         placeholder={placeholder}
         {...rest}
