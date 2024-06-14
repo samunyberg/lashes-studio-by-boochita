08f59bca39e156933cb025f632ed07dfcec3c6d2
samunyberg <samu.nyberg@gmail.com>
2024-06-14 19:55:03 +0300

    Add logout button to thank-you page

--- src/app/book/thank-you/page.tsx
+++ src/app/book/thank-you/page.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Button from '@/components/common/Button';
 import { MotionContainer } from '@/components/common/MotionContainer';
+import { signOut } from 'next-auth/react';
 import { BsCheckCircle } from 'react-icons/bs';
 
 interface Props {
@@ -10,7 +13,7 @@ interface Props {
 
 const ThankYouPage = ({ searchParams: { email } }: Props) => {
   return (
-    <div className='h-[calc(100vh-4rem)] pt-32'>
+    <div className='h-[calc(100vh-4rem)] px-6 pt-32'>
       <div className='flex flex-col items-center justify-center gap-4'>
         <MotionContainer
           initial={{ opacity: 0, scale: 0.5 }}
@@ -20,11 +23,18 @@ const ThankYouPage = ({ searchParams: { email } }: Props) => {
           <BsCheckCircle size={90} />
         </MotionContainer>
         <h1 className='text-lg font-semibold'>Thank You!</h1>
-        <p className='mb-5 px-3 text-center'>
+        <p className='mb-5 text-center'>
           Your booking is confirmed. A confirmation email was sent to{' '}
           <span className='font-semibold'>{email}</span>.
         </p>
-        <Button label='To Home Page' variant='primary' />
+        <div className='flex w-full flex-col gap-4'>
+          <Button label='To Home Page' variant='accent' />
+          <Button
+            label='Logout'
+            variant='primary'
+            onClick={() => signOut({ redirect: true, callbackUrl: '/' })}
+          />
+        </div>
       </div>
     </div>
   );
