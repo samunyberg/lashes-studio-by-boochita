45f4c730197e156621d3bd55cd9ef860eb7cfedf
samunyberg <samu.nyberg@gmail.com>
2024-06-14 19:44:13 +0300

    Redirect users upon signing in

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -2,6 +2,7 @@
 
 import { signIn } from 'next-auth/react';
 import Link from 'next/link';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 import Button from '../common/Button';
@@ -17,15 +18,22 @@ const LoginForm = ({
 }) => {
   const [credentials, setCredentials] = useState({ email: '', password: '' });
   const [error, setError] = useState('');
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
-  const login = async () => {
+  const login = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
     try {
       const result = await signIn('credentials', {
         ...credentials,
         redirect: false,
       });
       if (result?.error) setError('Invalid email or password');
-      if (result?.ok) toast.success(`Signed in as ${credentials.email}`);
+      if (result?.ok) {
+        toast.success(`Signed in as ${credentials.email}`);
+        router.push(searchParams.get('callbackUrl') || '/');
+      }
     } catch (error) {
       setError('Something went wrong. Please try again');
     }
@@ -64,7 +72,7 @@ const LoginForm = ({
       )}
       <AuthFormHeader subtitle='Login' />
       <FormError className='mb-4'>{error}</FormError>
-      <form className='mb-8 flex flex-col gap-6' action={login}>
+      <form className='mb-8 flex flex-col gap-6' onSubmit={login}>
         <Input
           type='text'
           name='email'
