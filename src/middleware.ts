eec090868b2180690fa44652c558c8a1a3854561
samunyberg <samu.nyberg@gmail.com>
2024-06-20 15:02:56 +0300

    Enhance middleware to protect admin routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,28 @@
-export { default } from 'next-auth/middleware';
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
 
-export const config = {
-  matcher: ['/book/:path*'],
-};
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req });
+
+  if (!token && req.nextUrl.pathname.startsWith('/book')) {
+    return NextResponse.redirect(
+      new URL('/auth/login?callbackUrl=/book', req.url)
+    );
+  }
+
+  if (token && req.nextUrl.pathname.startsWith('/auth')) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  if (token && !token.isAdmin && req.nextUrl.pathname.startsWith('/admin')) {
+    return NextResponse.redirect(new URL('/access-denied', req.url));
+  }
+
+  if (!token && req.nextUrl.pathname.startsWith('/admin')) {
+    return NextResponse.redirect(
+      new URL('/auth/login?callbackUrl=/admin', req.url)
+    );
+  }
+
+  return NextResponse.next();
+}
