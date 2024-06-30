db52a13b2bc7ccc70292ff82d81ccadf64bac8b9
samunyberg <samu.nyberg@gmail.com>
2024-06-30 17:02:52 +0300

    Build BookingSteps component

--- src/components/booking/BookingHeader.tsx
+++ src/components/booking/BookingHeader.tsx
@@ -1,41 +1,18 @@
-import { cn } from 'clsx-tailwind-merge';
-import { FaRegCheckCircle } from 'react-icons/fa';
-import { Step } from './BookingForm';
+import React from 'react';
 import BookingTime from './BookingTime';
+import { Step } from './BookingForm';
+import ProgressSteps from './ProgressSteps';
 
 interface Props {
   steps: Step[];
   currentStep: number;
 }
 
-const BookingHeader = ({ steps, currentStep }: Props) => {
+const BookingHeader = ({ currentStep, steps }: Props) => {
   return (
-    <div className='bg-bgSoft shadow'>
+    <div className='rounded-sm bg-bgSoft p-2 shadow'>
       <BookingTime />
-      <div className='relative flex h-20 w-full flex-row justify-between p-2'>
-        {steps.map((step) => (
-          <div key={step.stepNumber}>
-            <span
-              className={cn(
-                `flex items-center gap-1 px-1 text-sm text-opacity-60`,
-                {
-                  'text-opacity-90': step.stepNumber <= currentStep,
-                  'font-bold text-opacity-100 underline':
-                    step.stepNumber === currentStep,
-                }
-              )}
-            >
-              {step.label}
-              {step.stepNumber < currentStep && <FaRegCheckCircle />}
-            </span>
-            {step.stepNumber === currentStep && (
-              <div className='absolute bottom-0 left-0 w-full p-4 tracking-wide'>
-                {step.description}
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
+      <ProgressSteps currentStep={currentStep} steps={steps} />
     </div>
   );
 };
