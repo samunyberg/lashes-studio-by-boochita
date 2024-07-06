94e3c3bd13df4e73b596ee4af3b64c13c51dd602
samunyberg <samu.nyberg@gmail.com>
2024-07-06 15:53:59 +0300

    Modify PasswordStrength appearance

--- src/components/auth/PasswordStrength.tsx
+++ src/components/auth/PasswordStrength.tsx
@@ -4,23 +4,21 @@ const passwdStrengthLabels = ['Weak', 'Low', 'Medium', 'Strong'];
 
 const PasswordStrength = ({ passwdStrength }: { passwdStrength: number }) => {
   return (
-    <div className='px-4'>
-      <p className='mb-2 text-sm'>
-        Password strength:{' '}
+    <div className='flex items-center gap-4'>
+      <p className='whitespace-nowrap text-sm font-medium'>
+        Strength:{' '}
         <span className='font-semibold'>
           {passwdStrengthLabels[passwdStrength]}
         </span>
       </p>
-      <div className='flex h-2 overflow-hidden rounded-full border border-primary shadow-sm'>
-        <div
-          className={cn('h-full transition-all', {
-            'w-[15%] bg-red-400': passwdStrength === 0,
-            'w-[35%] bg-orange-300': passwdStrength === 1,
-            'w-[65%] bg-green-300': passwdStrength === 2,
-            'w-[100%] bg-green-400': passwdStrength === 3,
-          })}
-        ></div>
-      </div>
+      <div
+        className={cn('flex h-2 w-full rounded-full transition-all', {
+          'w-[15%] bg-red-400': passwdStrength === 0,
+          'w-[35%] bg-orange-300': passwdStrength === 1,
+          'w-[65%] bg-green-300': passwdStrength === 2,
+          'w-[100%] bg-green-400': passwdStrength === 3,
+        })}
+      ></div>
     </div>
   );
 };
