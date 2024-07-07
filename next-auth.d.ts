042a3d54eb3005411b4af8e82e558c64e899cfb2
samunyberg <samu.nyberg@gmail.com>
2024-07-07 16:27:34 +0300

    Change auth options

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,5 +1,5 @@
-import { DefaultSession, DefaultUser } from 'next-auth';
-import { DefaultJWT } from 'next-auth/jwt';
+import 'next-auth';
+import 'next-auth/jwt';
 
 declare module 'next-auth' {
   interface Session {
@@ -8,25 +8,25 @@ declare module 'next-auth' {
       firstName: string;
       lastName: string;
       email: string;
-      phone: string;
       isAdmin: boolean;
-    } & DefaultSession;
+    };
   }
 
-  interface User extends DefaultUser {
+  interface User {
     id: string;
     firstName: string;
     lastName: string;
-    isAdmin: boolean;
+    email: string;
     isAdmin: boolean;
   }
 }
 
 declare module 'next-auth/jwt' {
-  interface JWT extends DefaultJWT {
+  interface JWT {
     id: string;
     firstName: string;
     lastName: string;
+    email: string;
     isAdmin: boolean;
   }
 }
