2e76d1ef0777684bca1d29a781edeaa755593035
samunyberg <samu.nyberg@gmail.com>
2024-07-10 13:26:09 +0300

    Refactor ExpandedDay component

--- src/components/calendar/ExpandedDay.tsx
+++ src/components/calendar/ExpandedDay.tsx
@@ -1,8 +1,8 @@
 import useLanguage from '@/hooks/useLanguage';
 import { cn } from 'clsx-tailwind-merge';
 import { ReactNode } from 'react';
-import { CgCloseR } from 'react-icons/cg';
 import { FaRegCalendarCheck } from 'react-icons/fa';
+import { IoIosClose } from 'react-icons/io';
 import Portal from '../common/Portal';
 
 interface Props {
@@ -22,7 +22,7 @@ const ExpandedDay = ({
   const locale = `${currentLanguage}-FI`;
 
   const header = (
-    <div className='mb-2 flex items-center justify-between px-5 py-4'>
+    <div className='mb-2 flex items-center justify-between border-b border-gray-200 px-5 py-4'>
       <h1 className='flex items-center gap-2 rounded-sm text-lg font-medium'>
         <FaRegCalendarCheck className='size-5' />
         {selectedDate.toLocaleDateString(locale, {
@@ -32,7 +32,7 @@ const ExpandedDay = ({
           selectedDate.toLocaleDateString(locale)}
       </h1>
       <span onClick={() => onShowExpandedDay()}>
-        <CgCloseR size={25} />
+        <IoIosClose size={35} />
       </span>
     </div>
   );
@@ -40,16 +40,22 @@ const ExpandedDay = ({
   return (
     <Portal>
       <div
-        className={cn(
-          'fixed bottom-0 left-0 right-0 max-h-[0%] overflow-hidden overflow-y-visible border-t transition-all duration-200 ease-out',
-          {
-            'max-h-[80%]': showExpandedDay,
-          }
-        )}
+        className={cn('fixed bottom-0 left-0 right-0 transition-all', {
+          'top-0 bg-black/30': showExpandedDay,
+        })}
       >
-        <div className='z-50 h-full w-full overflow-hidden rounded-tl-2xl rounded-tr-2xl bg-white pb-20'>
-          {header}
-          {children}
+        <div
+          className={cn(
+            'fixed bottom-0 left-0 right-0 max-h-[0%] overflow-hidden overflow-y-visible transition-all duration-200 ease-out',
+            {
+              'max-h-[80%]': showExpandedDay,
+            }
+          )}
+        >
+          <div className='z-50 h-full w-full overflow-hidden rounded-tl-2xl rounded-tr-2xl bg-white pb-20'>
+            {header}
+            {children}
+          </div>
         </div>
       </div>
     </Portal>
