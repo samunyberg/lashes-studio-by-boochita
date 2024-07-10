6293f3ffd1ff27a676ffa14d0a0f631b21e136f7
samunyberg <samu.nyberg@gmail.com>
2024-07-10 13:22:53 +0300

    Refactor MyAppointments component

--- src/components/account/MyAppointments.tsx
+++ src/components/account/MyAppointments.tsx
@@ -1,18 +1,13 @@
 'use client';
 
+import { AppointmentWithService } from '@/app/lib/types';
 import useLanguage from '@/hooks/useLanguage';
-import { Appointment, Service, ServiceOption } from '@prisma/client';
 import { useRouter } from 'next/navigation';
-import { FaCheck, FaRegCalendarCheck, FaRegClock } from 'react-icons/fa';
 import Button from '../common/Button';
-
-export type AppointmentWithData = Appointment & {
-  service: Service | null;
-  serviceOption: ServiceOption | null;
-};
+import AppointmentPanel from '../common/appointments/AppointmentPanel';
 
 interface Props {
-  appointments: AppointmentWithData[];
+  appointments: AppointmentWithService[];
 }
 
 const MyAppointments = ({ appointments }: Props) => {
@@ -28,36 +23,11 @@ const MyAppointments = ({ appointments }: Props) => {
       ) : (
         <div className='mb-4 flex flex-col gap-3'>
           {appointments.map((app) => (
-            <div
+            <AppointmentPanel
               key={app.id}
-              className='border-l-4 border-accent bg-bgSoft px-4 py-4 font-medium shadow'
-            >
-              <div className='flex flex-col gap-1 px-4'>
-                <span className='flex items-center gap-2'>
-                  <FaRegCalendarCheck className='size-4' />
-                  {app.dateTime.toLocaleDateString(locale, {
-                    weekday: 'long',
-                    month: 'long',
-                    day: '2-digit',
-                  })}
-                </span>
-                <span className='flex items-center gap-2'>
-                  <FaRegClock className='size-4' />
-                  {app.dateTime.toLocaleTimeString(locale, {
-                    hour: '2-digit',
-                    minute: '2-digit',
-                  })}
-                </span>
-                <span className='flex items-center gap-2'>
-                  <FaCheck className='size-3' />
-                  {app.service?.name}
-                </span>
-                <span className='flex items-center gap-2'>
-                  <FaCheck className='size-3' />
-                  {app.serviceOption?.name}
-                </span>
-              </div>
-            </div>
+              appointment={app}
+              showClient={false}
+            />
           ))}
         </div>
       )}
