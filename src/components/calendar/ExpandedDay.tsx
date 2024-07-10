5dbf5cfcb1e1ae0a46f4065cd1ad6266036edcef
samunyberg <samu.nyberg@gmail.com>
2024-07-10 14:24:45 +0300

    Extract reusable modal component

--- src/components/calendar/ExpandedDay.tsx
+++ src/components/calendar/ExpandedDay.tsx
@@ -1,9 +1,8 @@
+import { formatDate } from '@/app/lib/dates';
 import useLanguage from '@/hooks/useLanguage';
-import { cn } from 'clsx-tailwind-merge';
 import { ReactNode } from 'react';
 import { FaRegCalendarCheck } from 'react-icons/fa';
-import { IoIosClose } from 'react-icons/io';
-import Portal from '../common/Portal';
+import Modal from '../common/Modal';
 
 interface Props {
   selectedDate: Date;
@@ -22,43 +21,23 @@ const ExpandedDay = ({
   const locale = `${currentLanguage}-FI`;
 
   const header = (
-    <div className='mb-2 flex items-center justify-between border-b border-gray-200 px-5 py-4'>
-      <h1 className='flex items-center gap-2 rounded-sm text-lg font-medium'>
-        <FaRegCalendarCheck className='size-5' />
-        {selectedDate.toLocaleDateString(locale, {
-          weekday: 'long',
-        }) +
-          ' ' +
-          selectedDate.toLocaleDateString(locale)}
-      </h1>
-      <span onClick={() => onShowExpandedDay()}>
-        <IoIosClose size={35} />
-      </span>
-    </div>
+    <span className='flex items-center gap-2 text-lg font-medium'>
+      <FaRegCalendarCheck className='size-5' />
+      {formatDate(selectedDate, locale, {
+        weekday: 'long',
+        month: 'numeric',
+        day: '2-digit',
+      })}
+    </span>
   );
 
   return (
-    <Portal>
-      <div
-        className={cn('fixed bottom-0 left-0 right-0 transition-all', {
-          'top-0 bg-black/30': showExpandedDay,
-        })}
-      >
-        <div
-          className={cn(
-            'fixed bottom-0 left-0 right-0 max-h-[0%] overflow-hidden overflow-y-visible transition-all duration-200 ease-out',
-            {
-              'max-h-[80%]': showExpandedDay,
-            }
-          )}
-        >
-          <div className='z-50 h-full w-full overflow-hidden rounded-tl-2xl rounded-tr-2xl bg-white pb-20'>
-            {header}
-            {children}
-          </div>
-        </div>
-      </div>
-    </Portal>
+    <Modal
+      isVisible={showExpandedDay}
+      onClose={onShowExpandedDay}
+      header={header}
+      content={children}
+    />
   );
 };
 
---
+++ src/components/common/Modal.tsx
@@ -0,0 +1,44 @@
+import { cn } from 'clsx-tailwind-merge';
+import { ReactNode } from 'react';
+import { IoIosClose } from 'react-icons/io';
+import Portal from './Portal';
+
+interface Props {
+  isVisible: boolean;
+  onClose: () => void;
+  header: ReactNode | string;
+  content: ReactNode;
+}
+
+const Modal = ({ isVisible, onClose, header, content }: Props) => {
+  return (
+    <Portal>
+      <div
+        className={cn('fixed bottom-0 left-0 right-0 transition-all', {
+          'top-0 bg-black/30': isVisible,
+        })}
+      >
+        <div
+          className={cn(
+            'fixed bottom-0 left-0 right-0 max-h-[0%] overflow-hidden overflow-y-visible transition-all duration-200 ease-out',
+            {
+              'max-h-[80%]': isVisible,
+            }
+          )}
+        >
+          <div className='z-50 h-full w-full overflow-hidden rounded-tl-2xl rounded-tr-2xl bg-white pb-20'>
+            <div className='flex items-center justify-between border-b border-gray-200 px-5 py-4'>
+              <div>{header}</div>
+              <span onClick={() => onClose()}>
+                <IoIosClose size={35} />
+              </span>
+            </div>
+            <div>{content}</div>
+          </div>
+        </div>
+      </div>
+    </Portal>
+  );
+};
+
+export default Modal;
