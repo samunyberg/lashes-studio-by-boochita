c43b3ef754ffae697c6c8b02a5d30e21e83b117c
samunyberg <samu.nyberg@gmail.com>
2024-07-12 16:01:22 +0300

    Clean up booking page

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -2,27 +2,17 @@ import BookingForm from '@/components/booking/BookingForm';
 import prisma from '../../prisma/client';
 
 const BookingPage = async () => {
-  const todayStart = new Date();
-  todayStart.setHours(0, 0, 0, 0);
-
-  const appointments = await prisma.appointment.findMany({
-    where: {
-      dateTime: {
-        gte: todayStart,
-      },
-    },
-    include: { service: true, serviceOption: true, client: true },
-  });
-
   const services = await prisma.service.findMany({
     include: { serviceOptions: true },
   });
 
   return (
     <div className='min-h-[calc(100vh-55px)]'>
-      <BookingForm appointments={appointments} services={services} />
+      <BookingForm services={services} />
     </div>
   );
 };
+
+export const dynamic = 'force-dynamic';
 
 export default BookingPage;
