7dbae0f3565742cb884ca65e230131ca81b297f5
samunyberg <samu.nyberg@gmail.com>
2024-07-17 14:11:44 +0300

    Change line chart appearance

--- src/components/admin/AppointmentChart.tsx
+++ src/components/admin/AppointmentChart.tsx
@@ -1,44 +1,17 @@
-'use client';
-
-import { formatDate } from '@/app/lib/dates';
-import useLocale from '@/hooks/useLocale';
-import {
-  CartesianGrid,
-  Line,
-  LineChart,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from 'recharts';
+import Chart from './Chart';
+import ChartHeader from './ChartHeader';
 
 interface Props {
-  data: { month: string; count: number }[];
+  fetchData: () => Promise<{ month: string; count: number }[]>;
 }
 
-const AppointmentLineChart = ({ data }: Props) => {
-  const locale = useLocale();
+const AppointmentLineChart = async ({ fetchData }: Props) => {
+  const data = await fetchData();
 
   return (
     <div className='h-full'>
-      <h1 className='sticky top-0 mb-5 border-b border-accent bg-bgMain text-base font-semibold uppercase'>
-        Booked {formatDate(new Date(), locale, { year: 'numeric' })}
-      </h1>
-      <ResponsiveContainer width='100%' height='100%'>
-        <LineChart data={data}>
-          <CartesianGrid stroke='rgba(0, 0, 0, 0.1)' />
-          <XAxis dataKey='month' stroke='#524237' />
-          <YAxis stroke='#524237' />
-          <Tooltip />
-          <Line
-            strokeWidth={2}
-            type='monotone'
-            dataKey='count'
-            stroke='#c9a489'
-            activeDot={{ r: 8 }}
-          />
-        </LineChart>
-      </ResponsiveContainer>
+      <ChartHeader />
+      <Chart data={data} />
     </div>
   );
 };
