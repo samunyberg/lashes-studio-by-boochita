e041a04c2221bd0d1a0a7f5306ef5e225e617e6a
samunyberg <samu.nyberg@gmail.com>
2024-07-18 11:06:16 +0300

    Refactor book page

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -1,14 +1,17 @@
 import BookingForm from '@/components/booking/BookingForm';
 import prisma from '../../prisma/client';
+import { getUpcomingAppointments } from '../lib/db/appointments';
 
 const BookingPage = async () => {
   const services = await prisma.service.findMany({
     include: { serviceOptions: true },
   });
 
+  const appointments = await getUpcomingAppointments();
+
   return (
     <div className='min-h-[calc(100vh-55px)]'>
-      <BookingForm services={services} />
+      <BookingForm services={services} appointments={appointments} />
     </div>
   );
 };
