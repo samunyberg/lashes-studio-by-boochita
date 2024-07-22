1cbf514de6ee70706ac7692124034002f8ef973b
samunyberg <samu.nyberg@gmail.com>
2024-07-22 10:29:03 +0300

    Refactor Today component

--- src/components/admin/Today.tsx
+++ src/components/admin/Today.tsx
@@ -2,6 +2,8 @@
 
 import { formatDate } from '@/app/lib/dates';
 import { AppointmentWithAllData } from '@/app/lib/types';
+import useLocale from '@/hooks/useLocale';
+import { useRouter } from 'next/navigation';
 import AppointmentPanel from '../common/appointments/AppointmentPanel';
 import Button from '../common/Button';
 import Label from '../common/Label';
@@ -12,11 +14,14 @@ interface Props {
 }
 
 const Today = ({ appointments }: Props) => {
+  const locale = useLocale();
+  const router = useRouter();
+
   return (
     <div>
       <h1 className='mb-3 border-b border-accent bg-bgMain text-base font-semibold uppercase'>
         <Label labelId='today' />,{' '}
-        {formatDate(new Date(), 'en-FI', {
+        {formatDate(new Date(), locale, {
           day: '2-digit',
           month: 'long',
         })}
@@ -24,7 +29,7 @@ const Today = ({ appointments }: Props) => {
       <div className='flex flex-col gap-5'>
         {appointments.length === 0 ? (
           <Panel className='px-4 py-3'>
-            No appointments for today. Enjoy your day off!
+            <Label labelId='now_appointments_today' />
           </Panel>
         ) : (
           <div className='flex flex-col gap-2'>
@@ -37,8 +42,12 @@ const Today = ({ appointments }: Props) => {
             ))}
           </div>
         )}
-        <Button variant='accent' className='lg:w-fit lg:self-end'>
-          View all
+        <Button
+          variant='accent'
+          className='lg:w-fit lg:self-end'
+          onClick={() => router.push('/admin/appointments')}
+        >
+          <Label labelId='view_all' />
         </Button>
       </div>
     </div>
