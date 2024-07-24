b284ebbe4477c2160d594362b9ecfa0160eae041
samunyberg <samu.nyberg@gmail.com>
2024-07-24 12:04:03 +0300

    Changes to AppointmentStatusBadge

--- src/components/common/appointments/AppointmentStatusBadge.tsx
+++ src/components/common/appointments/AppointmentStatusBadge.tsx
@@ -2,17 +2,18 @@ import { Status } from '@prisma/client';
 import { cn } from 'clsx-tailwind-merge';
 import Label from '../Label';
 
-const AppointmentStatusBadge = ({ status }: { status: Status }) => {
+interface Props {
+  status: Status;
+}
+
+const AppointmentStatusBadge = ({ status }: Props) => {
   return (
     <div
-      className={cn(
-        'flex w-28 items-center justify-center rounded-full border-2 py-1',
-        {
-          'border-red-400': status === 'BOOKED',
-          'border-green-400': status === 'AVAILABLE',
-          'border-orange-300': status === 'UNAVAILABLE',
-        }
-      )}
+      className={cn('w-[95px] rounded-md border-2 bg-bgSoft py-1 text-center', {
+        'border-red-400': status === 'BOOKED',
+        'border-green-400': status === 'AVAILABLE',
+        'border-orange-300': status === 'UNAVAILABLE',
+      })}
     >
       <p className='text-sm font-medium'>
         <Label labelId={status.toLowerCase()} />
