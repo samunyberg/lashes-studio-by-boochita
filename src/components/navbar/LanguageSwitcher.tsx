da1d30e7462b2e3be5800ba6009ac20ccc1863b4
samunyberg <samu.nyberg@gmail.com>
2024-07-24 12:01:59 +0300

    Build a custom langugage switcher

--- src/components/navbar/LanguageSwitcher.tsx
+++ src/components/navbar/LanguageSwitcher.tsx
@@ -1,22 +1,44 @@
+import useClickOutside from '@/hooks/useClickOutside';
 import useLanguage from '@/hooks/useLanguage';
 import { Language } from '@/providers/language/LanguageProvider';
+import { useRef, useState } from 'react';
+import { MdLanguage } from 'react-icons/md';
 
 const LanguageSwitcher = () => {
   const { changeLanguage, currentLanguage } = useLanguage();
+  const [showOptions, setShowOptions] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useClickOutside(ref, () => setShowOptions(false));
+
+  const option = (language: Language) => (
+    <span
+      className='cursor-pointer rounded-sm pl-2 transition-all lg:hover:bg-accent lg:hover:text-white'
+      onClick={() => {
+        changeLanguage(language);
+        setShowOptions(false);
+      }}
+    >
+      {language}
+    </span>
+  );
 
   return (
-    <select
-      className='cursor-pointer rounded-sm bg-bgSoft text-sm p-1'
-      defaultValue={currentLanguage}
-      onChange={(event) => changeLanguage(event.target.value as Language)}
-    >
-      <option className='cursor-pointer hover:bg-accent' value='fi'>
-        Suomi
-      </option>
-      <option className='cursor-pointer' value='en'>
-        English
-      </option>
-    </select>
+    <div ref={ref} className='relative font-medium tracking-wide'>
+      <div
+        className='flex w-[50px] cursor-pointer items-center justify-center gap-1 rounded-sm bg-bgSoft'
+        onClick={() => setShowOptions(!showOptions)}
+      >
+        <span>{currentLanguage}</span>
+        <MdLanguage />
+      </div>
+      {showOptions && (
+        <div className='absolute inset-x-0 flex flex-col gap-1 rounded-bl-sm rounded-br-sm bg-bgSoft p-1'>
+          {option('fi')}
+          {option('en')}
+        </div>
+      )}
+    </div>
   );
 };
 
