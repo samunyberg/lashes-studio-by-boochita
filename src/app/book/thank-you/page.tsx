87d268d450b8175f924a226900eb0a5187d6fe6a
samunyberg <samu.nyberg@gmail.com>
2024-07-31 14:47:37 +0300

    Build animated checkmark

--- src/app/book/thank-you/page.tsx
+++ src/app/book/thank-you/page.tsx
@@ -1,12 +1,11 @@
 'use client';
 
 import Button from '@/components/common/Button';
+import CheckMark from '@/components/common/checkmark/CheckMark';
 import Label from '@/components/common/Label';
-import { MotionContainer } from '@/components/common/MotionContainer';
 import Panel from '@/components/common/Panel';
 import { signOut } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import { BsCheckCircle } from 'react-icons/bs';
 
 interface Props {
   searchParams: {
@@ -19,17 +18,8 @@ const ThankYouPage = ({ searchParams: { email } }: Props) => {
 
   return (
     <div className='h-[calc(100vh-55px)] pt-32 md:mx-auto md:max-w-[350px] lg:max-w-[450px]'>
-      <Panel className='flex flex-col items-center justify-center gap-4 py-12 px-5'>
-        <MotionContainer
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.8 }}
-        >
-          <BsCheckCircle size={90} />
-        </MotionContainer>
-        <h1 className='text-lg font-semibold'>
-          <Label labelId='thank_you' />
-        </h1>
+      <Panel className='flex flex-col items-center justify-center gap-4 px-5 py-12'>
+        <CheckMark text={<Label labelId='thank_you' />} />
         <p className='mb-5 text-center font-medium'>
           <Label labelId='booking_confirmed' />{' '}
           <span className='font-semibold'>{email}</span>.
