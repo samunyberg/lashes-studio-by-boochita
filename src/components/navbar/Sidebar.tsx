3b2514ab8aa1b86251685f9aac1c3325ade821ca
samunyberg <samu.nyberg@gmail.com>
2024-08-02 00:33:57 +0300

    Improve Sidebar

--- src/components/navbar/Sidebar.tsx
+++ src/components/navbar/Sidebar.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import useClickOutside from '@/hooks/useClickOutside';
+import { cn } from 'clsx-tailwind-merge';
+import { useRef } from 'react';
 import { MdClose } from 'react-icons/md';
 import LanguageSwitcher from './LanguageSwitcher';
 import NavLinks from './NavLinks';
@@ -8,20 +13,24 @@ interface Props {
 }
 
 const Sidebar = ({ isOpen, onToggleSidebar }: Props) => {
+  const sidebarRef = useRef(null);
+  useClickOutside(sidebarRef, () => onToggleSidebar());
+
   return (
     <aside
-      className={`${
-        isOpen ? 'left-0 w-screen' : '-left-52 max-w-0'
-      } fixed top-0 z-[999] flex h-screen flex-row transition-all duration-300 lg:hidden`}
+      ref={sidebarRef}
+      className={cn(
+        'fixed inset-y-0 -left-[45%] z-[999] w-[45%] bg-bgSoft px-5 py-4 shadow-md backdrop-blur-lg transition-all',
+        {
+          'left-0': isOpen,
+        }
+      )}
     >
-      <div className='w-[45%] bg-bgSoft px-5 py-4 shadow-md backdrop-blur-lg'>
-        <div className='flex justify-between'>
-          <MdClose size={30} onClick={() => onToggleSidebar()} />
-          <LanguageSwitcher />
-        </div>
-        <NavLinks onLinkClick={onToggleSidebar} />
+      <div className='flex justify-between'>
+        <MdClose size={30} onClick={() => onToggleSidebar()} />
+        <LanguageSwitcher />
       </div>
-      <div className='flex-1' onClick={() => onToggleSidebar()}></div>
+      <NavLinks onLinkClick={onToggleSidebar} />
     </aside>
   );
 };
